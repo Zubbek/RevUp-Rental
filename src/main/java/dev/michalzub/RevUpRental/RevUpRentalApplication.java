7065df023b4b26dc0df2e18b1e663c8424f17aec
Zubbek <michal.zub@outlook.com>
2024-03-25 16:49:32 +0100

    [backend]- Building core of the application

---
+++ src/main/java/dev/michalzub/RevUpRental/RevUpRentalApplication.java
@@ -0,0 +1,13 @@
+package dev.michalzub.RevUpRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevUpRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevUpRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/michalzub/RevUpRental/RevUpRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.michalzub.RevUpRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevUpRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
