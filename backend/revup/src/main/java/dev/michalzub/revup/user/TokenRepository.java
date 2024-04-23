90423763ee2cbd2ce25e548b571a32efdaedaabf
Zubbek <michal.zub@outlook.com>
2024-04-23 17:30:24 +0200

    [backend] deleting Token class-no usage because of resigning email notofication

--- backend/revup/src/main/java/dev/michalzub/revup/user/TokenRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.michalzub.revup.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface TokenRepository extends JpaRepository<Token, Integer> {
-    Optional<Token> findByToken(String token);
-}
