6a2459949589d000752b3decb08803c7d0fcb146
Zubbek <michal.zub@outlook.com>
2024-04-29 16:47:29 +0200

    [backend] adding another entities to application

---
+++ backend/revup/src/main/java/dev/michalzub/revup/favourites/FavouritesModel.java
@@ -0,0 +1,19 @@
+package dev.michalzub.revup.favourites;
+
+import dev.michalzub.revup.motorcycle.MotorcycleModel;
+import dev.michalzub.revup.user.UserModel;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "favourites")
+public class FavouritesModel {
+  @Id
+  @GeneratedValue
+  private Integer id;
+  @ManyToMany(mappedBy = "favourites")
+  private List<UserModel> users;
+  @ManyToMany(mappedBy = "favourites")
+  private List<MotorcycleModel> motorcycles;
+}
---
+++ backend/revup/src/main/java/dev/michalzub/revup/reservation/ReservationModel.java
@@ -0,0 +1,19 @@
+package dev.michalzub.revup.reservation;
+
+import dev.michalzub.revup.motorcycle.MotorcycleModel;
+import dev.michalzub.revup.user.UserModel;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "reservations")
+public class ReservationModel {
+  @Id
+  @GeneratedValue
+  private Integer id;
+  @OneToOne(mappedBy = "reservation")
+  private ReservationDetailsModel reservationDetails;
+  @ManyToOne
+  private UserModel user;
+  @ManyToOne
+  private MotorcycleModel motorcycle;
+}
