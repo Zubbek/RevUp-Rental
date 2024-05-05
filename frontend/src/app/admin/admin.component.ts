bf8454de24d208281c37a5d31c0526b2d8c85366
Zubbek <michal.zub@outlook.com>
2024-05-05 18:29:51 +0200

    [Frontend] updating admin panel to manage users from database(deleting or setting unenabled)

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import USERS from "./users";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
-
+  users = USERS;
 }
--- frontend/src/app/motorcycles/motorcycles.component.ts
+++ frontend/src/app/motorcycles/motorcycles.component.ts
@@ -2,26 +2,72 @@ import {Component, OnInit} from '@angular/core';
 import MOTORCYCLES from "./motorcycle-data";
 import {ActivatedRoute} from "@angular/router";
 import {CategoryService} from "../services/category/category.service";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-motorcycles',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './motorcycles.component.html',
   styleUrl: './motorcycles.component.css'
 })
 export class MotorcyclesComponent implements OnInit {
   categoryName: string = '';
+  image: string = '';
   motorcycles: any[] = [];
+  isCategoriesFetched: boolean = false;
 
   constructor(private route: ActivatedRoute, private categoryService: CategoryService) { }
 
+  // ngOnInit(): void {
+  //   if (!this.isCategoriesFetched) {
+  //     this.route.params.subscribe(async params => {
+  //       this.categoryName = params['categoryName'];
+  //       console.log(this.categoryName)// Odczytaj nazwę kategorii z parametrów ścieżki
+  //       this.motorcycles = await this.categoryService.getModels(this.categoryName);
+  //       // for (const motorcycle of this.motorcycles) {
+  //       //   // Sprawdź, czy articleID jest zdefiniowany
+  //       //   if (motorcycle.articleID) {
+  //       //     const image = await this.categoryService.fetchImage(motorcycle.articleID);
+  //       //     if (image) {
+  //       //       motorcycle.image = image; // Przypisz obrazek do pola image w obiekcie motorcycle
+  //       //     }
+  //       //   }
+  //       // }
+  //       this.image = await this.categoryService.fetchImage("804882")
+  //       console.log(this.image)
+  //       console.log(this.motorcycles);
+  //       this.isCategoriesFetched = true;
+  //     });
+  //   }
+  // }
+
   ngOnInit(): void {
-    this.route.params.subscribe(async params => {
-      this.categoryName = params['categoryName'];
-      console.log(this.categoryName)// Odczytaj nazwę kategorii z parametrów ścieżki
-      this.motorcycles = await this.categoryService.getModels(this.categoryName);
-      console.log(this.motorcycles);
-    });
+    if (!this.isCategoriesFetched) {
+      this.route.params.subscribe(async params => {
+        this.categoryName = params['categoryName'];
+        console.log(this.categoryName);// Odczytaj nazwę kategorii z parametrów ścieżki
+        this.motorcycles = await this.categoryService.getModels(this.categoryName);
+
+        // Zbierz wszystkie obrazki asynchronicznie
+        const fetchImagePromises = this.motorcycles.map(async (motorcycle) => {
+          if (motorcycle.articleID) {
+            const image = await this.categoryService.fetchImage(motorcycle.articleID);
+            console.log(image);
+            if (image) {
+              motorcycle.image = image; // Przypisz obrazek do pola image w obiekcie motorcycle
+            }
+          }
+        });
+
+        // Poczekaj na zakończenie wszystkich operacji fetchImage
+        await Promise.all(fetchImagePromises);
+
+        console.log(this.motorcycles);
+        this.isCategoriesFetched = true;
+      });
+    }
   }
 }
